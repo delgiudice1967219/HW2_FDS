78285720e3fdf9608831b2cef87f136f727942ea
xavidg7 <x.delgiudice.data-driven.ai>
2024-11-24 21:12:57 +0100

    added final comments

--- libs/models/custom_cnn.py
+++ libs/models/custom_cnn.py
@@ -11,13 +11,13 @@ class CustomCNN(nn.Module):
         # Batch Normalization
         self.conv1_bn = nn.BatchNorm2d(16)
 
-        # The rectified linear unit function, used as our activation function for the custom model
+        # The rectified linear unit function, used as our activation function
         self.relu1 = nn.ReLU()
 
         # Dropout with a probability of 0.1 for every neuron to be deactivated
         self.dropout1 = nn.Dropout(0.1)
 
-        # Max Pool with stride 2, so we halve the matrix dimension every time we apply this technique
+        # Max Pool with stride 2, so we halve the images dimensions every time we apply it
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
@@ -35,22 +35,33 @@ class CustomCNN(nn.Module):
         self.dropout4 = nn.Dropout(0.2)
         self.relu4 = nn.ReLU()
 
-        #Starting from images of 32x32 dimension and 3 channels(RGB) as input, we apply twice a MaxPool function with stride equal to 2,
-        #we divide by 4 the starting matrix, resulting in a 8x8. In the meantime, we increase the output channels at every convolution
-        #to extract the maximum information available, so we arrive at the last convolution with a number of channels at the output of 128.
-        #From there, we apply a first fully connection to merge all the infos in 64 channels of output, and in the end we apply a second
-        #fully connection to return the 10 channels of the 10 classes of the dataset we want to classify in our network.
         self.fc1 = nn.Linear(128* 8 * 8, 64)
         self.fc2 = nn.Linear(64, 10)
 
     def forward(self, x):
-        #Our scheme for this network was a double CONV-ReLU-CONV-ReLU-Pool, so in the end
-        #we had 4 layers in total.
+
+        # Our scheme for this network was a double CONV-ReLU-CONV-ReLU-Pool, so in the end
+        # we had 4 layers in total.
+        
+        # Starting with images of 32x32x3 (HeigthxWeigthxChannels) as input
         x = self.relu1(self.dropout1(self.conv1_bn(self.conv1(x))))
+        
+        # After our first CONV-ReLU-CONV-ReLU-Pool sequence we obtain images of 16x16x32 (HxWxC)
         x = self.pool(self.relu2(self.dropout2(self.conv2_bn(self.conv2(x)))))
+        
         x = self.relu3(self.dropout3(self.conv3_bn(self.conv3(x))))
+        
+        # After our second CONV-ReLU-CONV-ReLU-Pool sequence we obtain images of 8x8x128 (HxWxC)
         x = self.pool(self.relu4(self.dropout4(self.conv4_bn(self.conv4(x)))))
+        
+        # Stretch to 1 x 8192
         x = x.view(-1, 128 * 8 * 8)
+
+        # Fully Connected Layer that takes in input 1 x 8192 tensor
+        # and gives us in output 1 x 64 tensor
         x = self.fc1(x)
+
+        # Fully Connected Layer that takes in input 1 x 64 tensor
+        # and gives us in output 1 x 10 tensor        
         x = self.fc2(x)
         return x
--- libs/models/poor_cnn.py
+++ libs/models/poor_cnn.py
@@ -4,23 +4,36 @@ import torch
 class PoorPerformingCNN(nn.Module):
     def __init__(self):
         super(PoorPerformingCNN, self).__init__()
-        ##############################
-        ###     CHANGE THIS CODE   ###
-        ##############################
 
-        # Which changes we did?
         self.conv1 = nn.Conv2d(3, 4, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
+        # Update the input to match the conv1 output 
         self.conv2 = nn.Conv2d(4, 8, kernel_size=3, stride=1, padding=1)
         self.relu2 = nn.ReLU()
 
-        self.fc1 = nn.Linear(8 * 8 * 8, 64)
+        # Update the input and output dimensions
+        # The input dimensions after all the previous operation
+        # are going to be 8x8x8 (HxWxC)
+        # The output is 10 to match the number of classes
+        self.fc1 = nn.Linear(8 * 8 * 8, 10)
 
     def forward(self, x):
+        
+        # We start with input images of 32x32x3
+        # After the first conv and pool we get images
+        # 16x16x4 (HxWxC)
         x = self.pool(self.relu1(self.conv1(x)))
+        
+        # After the second conv and pool we get images
+        # 8x8x8 (HxWxC)
         x = self.pool(self.relu2(self.conv2(x)))
+        
+        # Stretch to 1 x 512
         x = x.view(-1, 8 * 8 * 8)
+
+        # Fully Connected Layer that takes in input a 1 x 512 tensor
+        # and gives us in output a 1 x 10 tensor 
         x = self.fc1(x)
         return x
